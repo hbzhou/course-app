6cce7371fe5886ca632fe2e5e19e3d66222b683f
Jeremy Zhou <jeremy_zhou@JeremydeMacBook-Air.local>
2026-01-24 21:30:21 +0800

    adding context-path /api

--- src/main/kotlin/com/itsz/setup/controller/RoleController.kt
+++ src/main/kotlin/com/itsz/setup/controller/RoleController.kt
@@ -8,7 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/roles")
+@RequestMapping("/roles")
 class RoleController(private val roleService: RoleService) {
 
     @GetMapping
--- src/main/kotlin/com/itsz/setup/controller/UserController.kt
+++ src/main/kotlin/com/itsz/setup/controller/UserController.kt
@@ -8,7 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 class UserController(private val userService: UserService) {
 
     @GetMapping
