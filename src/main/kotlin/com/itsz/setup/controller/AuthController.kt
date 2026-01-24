4d6009af9c47da961ed9bed75acab8b36959e884
Jeremy Zhou <jeremy_zhou@JeremydeMacBook-Air.local>
2026-01-24 21:30:21 +0800

    adding context-path /api

--- src/main/kotlin/com/itsz/setup/controller/AuthController.kt
+++ src/main/kotlin/com/itsz/setup/controller/AuthController.kt
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping()
 class AuthController(
     private val authenticationManager: AuthenticationManager,
     private val userDetailsService: UserDetailsService,
