fd2c387caf60bf7ff98c98444ca5ca4fd92e4f10
Jeremy Zhou <jeremy_zhou@Mac.local>
2026-02-01 22:16:09 +0800

    fix the api when create author and course

--- src/main/kotlin/com/itsz/app/domain/Author.kt
+++ src/main/kotlin/com/itsz/app/domain/Author.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "author")
 data class Author(
-    @Id
-    val id: String,
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: String?,
     val name: String
 )
