d2e3df59dbbd2d91a0f39715d17b58b378ee4abb
Jeremy Zhou <jeremy_zhou@epam.com>
2026-04-21 21:34:09 +0800

    fix api UT

--- api/src/test/kotlin/com/itsz/app/config/TestSecurityConfigDisabler.kt
+++ api/src/test/kotlin/com/itsz/app/config/TestSecurityConfigDisabler.kt
@@ -1,6 +1,7 @@
 package com.itsz.app.config
 
 import com.itsz.app.auth.oauth2.OAuth2AuthorityMapper
+import com.itsz.app.auth.oauth2.OAuth2ProviderProfile
 import com.itsz.app.auth.oauth2.OAuth2ProviderResolver
 import com.itsz.app.auth.oauth2.ProviderAwareJwtAuthenticationConverter
 import org.mockito.Mockito
@@ -94,7 +95,10 @@ class TestSecurityConfigDisabler {
     @Bean
     @Primary
     fun mockOAuth2ProviderResolver(): OAuth2ProviderResolver {
-        return Mockito.mock(OAuth2ProviderResolver::class.java)
+        val mock = Mockito.mock(OAuth2ProviderResolver::class.java)
+        val defaultProfile = OAuth2ProviderProfile("azure", "Azure AD", "https://login.microsoftonline.com/common/v2.0")
+        Mockito.`when`(mock.defaultProfile()).thenReturn(defaultProfile)
+        return mock
     }
 
     @Bean
